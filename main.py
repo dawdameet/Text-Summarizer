388af3023ad391da4ace2a0a583884716f14f5c3
Meet Dawda <94282163+dawdameet@users.noreply.github.com>
2025-02-03 21:06:40 +0530

    Added flask

--- main.py
+++ main.py
@@ -2,21 +2,35 @@ import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-text = """
-A purple octopus wearing a top hat strolled through the bustling marketplace, humming a tune that sounded suspiciously like a mix of jazz and whale songs. Nearby, a group of toaster-like robots were arguing over the best way to butter toast, while a llama on roller skates zoomed past, shouting something about pineapple pizza being the ultimate truth. In the corner of the market, a wizard was selling enchanted cheese graters that could supposedly predict the weather, but only if you asked them nicely. Meanwhile, the clouds above decided to rain tiny marshmallows, which everyone agreed was both delightful and extremely inconvenient.
-"""
-sntc = nltk.sent_tokenize(text)
-vctrzr = TfidfVectorizer()
-tfidf = vctrzr.fit_transform(sntc)
-avgv = np.mean(tfidf.toarray(), axis=0)
-sim = []
-for i in range(len(sntc)):
-    sentence_vector = tfidf[i].toarray().flatten()
-    similarity = cosine_similarity([sentence_vector], [avgv])[0][0]
-    sim.append(similarity)
-rnk = [sentence for _, sentence in sorted(zip(sim, sntc), reverse=True)]
-#k-statements
-tks = 2
-smry = " ".join(rnk[:tks])
-print("Summary:")
-print(smry)
+from flask import Flask,request, jsonify
+
+def summarize(text):
+    sntc = nltk.sent_tokenize(text)
+    vctrzr = TfidfVectorizer()
+    tfidf = vctrzr.fit_transform(sntc)
+    avgv = np.mean(tfidf.toarray(), axis=0)
+    sim = []
+    for i in range(len(sntc)):
+        sentence_vector = tfidf[i].toarray().flatten()
+        similarity = cosine_similarity([sentence_vector], [avgv])[0][0]
+        sim.append(similarity)
+    rnk = [sentence for _, sentence in sorted(zip(sim, sntc), reverse=True)]
+    tks = 2
+    smry = " ".join(rnk[:tks])
+
+    return smry
+
+
+
+text=""""""
+app=Flask(__name__)
+
+@app.route("/api/summary", methods=['POST'])
+def get():
+    requestData=request.get_json()
+    text=requestData['text']
+    return summarize(text)
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
